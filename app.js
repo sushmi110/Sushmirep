c3f25c5e86cb481628a2577f066049681cee6048
sushmi110 <126650987+sushmi110@users.noreply.github.com>
2023-11-17 14:25:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+(function() {
+
+    let screen = document.querySelector('.screen');
+    let buttons =document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    buttons.forEach(function(button))
+
+
+})();
